d1f251e2b55d064f43360214461e4bb76298de92
Alexandre Santos Work <alexandre@diariodecodiogs.info>
2010-03-18 17:46:18 -0300

    ConnectionFactory created, miss create code for connection DB

--- classes/ConnectionFactory.class.php
+++ classes/ConnectionFactory.class.php
@@ -1,20 +1,68 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
-  * Descrição...
+  * Fabrica conexões e retorna um objeto PDO já conectado
+  * Implementa o padrão Singleton, obtenha a instância da classe
+  * pelo método "getInstance"
   *
-  * @package AWU
-  * @copyright Copyright (c) SeuNome
+  * @package AWUFiv
+  * @copyright Copyright (c) Alexandre Santos
   * @license http://www.opensource.org/licenses/gpl-3.0.html GNU Public License 3.0
   * @version 1.0
-  * @author SeuNome <email@example.com>
+  * @author Alexandre Santos <alexandre@diariodecodigos.info>
   *
   */
-class ConnectionFactoryclass {
-    //put your code here
-}
+class ConnectionFactory {
+        /**
+         * Armazena o objeto da própria classe ConnectionFactory
+         * @var object
+         */
+        private static $instance;
+
+        /**
+         * Modifica o acesso ao construtor para private
+         */
+        private function __construct()
+        {
+            echo "Previne a criação da instância modificando a visibilidade para private.";
+        }
+
+        /**
+         * Retorna um objeto da classe ConnectionFactory
+         * @return ConnectionFactory object
+         */
+        public static function getInstance()
+        {
+            if(!isset(self::$instance))
+            {
+                $c = __CLASS__;
+                self::$instance = new $c;
+            }
+
+            return self::$instance;
+        }
+
+        /**
+         * Evita a clonagem do objeto
+         */
+        public function __clone(){
+            throw new Exception("É proibido clonar esse objeto, obtenha um novo pelo método getInstance()");
+        }
+
+        /**
+         * Retorna um objeto PDO com a conexão ativa ao antigo schema
+         * @return PDO object
+         */
+        public function getConnectionOld()
+        {
+
+        }
+
+        /**
+         * Retorna um objeto PDO com conexão ativa ao novo schema
+         * @return PDO object
+         */
+        public function getConnectionNew(){
+
+        }
+    }
 ?>
